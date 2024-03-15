f923bfc3550a7df308c1ddcfbd74fe23cdba1426
Bruno Duarte <brunosduarte@hotmail.com>
2024-03-15 15:18:43 -0300

    feature: included vercel analytics on the project

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from '@/App'
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Analytics/>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider> 
