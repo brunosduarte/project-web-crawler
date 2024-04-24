bf7a53e105e8c7698331ac9ac1b67e75f2b29f3c
Bruno Duarte <brunosduarte@hotmail.com>
2024-04-24 16:07:11 -0300

    refactor: adjusting time on loading animation

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -2,12 +2,8 @@ export function Loading() {
   return (
     <div className="flex h-6 w-10 items-center justify-center gap-2 overflow-hidden">
       <div className="h-2 w-4 animate-pulse rounded-full bg-blue-200 [animation-delay:0.25s]"></div>
-      <div className="h-2 w-4 animate-pulse rounded-full bg-blue-200 [animation-delay:-0.25s]"></div>
-      <div className="h-2 w-4 animate-pulse rounded-full bg-blue-200"></div>
+      <div className="h-2 w-4 animate-pulse rounded-full bg-blue-200 [animation-delay:0.5s]"></div>
+      <div className="h-2 w-4 animate-pulse rounded-full bg-blue-200 [animation-delay:0.75s]"></div>
     </div>
   )
 }
-// import { CircleNotch } from 'phosphor-react'
-// <div className="flex h-6 w-6 items-center justify-center overflow-hidden">
-//   <CircleNotch weight="bold" className="h-4 w-4 animate-spin" />
-// </div>
