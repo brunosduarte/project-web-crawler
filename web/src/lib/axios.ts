b63bd4ce89ca90e8054c97d1e1d91bebea0aa3c8
Bruno Duarte <brunosduarte@hotmail.com>
2024-04-29 02:48:17 -0300

    fix: fixing axios errors

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://project-web-crawler.onrender.com',
+  baseURL: 'https://sitemapper-v1-2.onrender.com',
+  // baseURL: 'https://project-web-crawler.onrender.com',
   // baseURL: 'http://localhost:3000',
 })
